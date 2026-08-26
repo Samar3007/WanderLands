a702d7c22ed5fd8dcebc15d912464c500341fa87
Samarjeet Baliyan <samar3007@Samarjeets-MacBook-Air.local>
2026-08-26 21:20:02 +0530

    Latest edits for the site

--- app.js
+++ app.js
@@ -24,7 +24,10 @@ const reviewRoutes = require('./routes/reviews');
 const MongoDBStore = require("connect-mongo")(session);
 
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL;
+if (!dbUrl) {
+    throw new Error('DB_URL environment variable is required');
+}
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
